131b1b171de12f3f919aecf1e841d0fd5ac62926
bwehrly859 <bwehrly859@gmail.com>
2023-02-12 01:40:43 -0500

    fix animate on initial render

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,29 @@
 import "./style.css";
 import { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion";
 
 const Header = () => {
-
-  const { isDarkTheme, handleTheme } = useContext(ThemeContext)
+  const { isDarkTheme, handleTheme } = useContext(ThemeContext);
 
   return (
     <header>
       <h1>Quicktionary</h1>
-      <button className="theme" onClick={handleTheme} onMouseDown={e => e.preventDefault()}>
+      <button
+        className="theme"
+        onClick={handleTheme}
+        onMouseDown={(e) => e.preventDefault()}
+      >
         <div className={isDarkTheme ? "toggle" : "toggle flipped"}>
-          <motion.div 
-            className="ripple"
-            initial={{ scale: 1, opacity: 1}}
-            animate={{ scale: 5, opacity: 0}}
-            transition={{ duration: .3}}
-            key={isDarkTheme}
-        />
+          <AnimatePresence initial={false}>
+            <motion.div
+              className="ripple"
+              initial={{ scale: 1, opacity: 1 }}
+              animate={{ scale: 5, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              key={isDarkTheme}
+            />
+          </AnimatePresence>
         </div>
       </button>
     </header>
